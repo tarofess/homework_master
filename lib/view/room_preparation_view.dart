c62656c7afd44e4ed0d40a6bb9519542801c6575
taro <tarofess@gmail.com>
2024-08-19 15:41:17 +0900

    room_preparation_viewのリファクタリング

--- lib/view/room_preparation_view.dart
+++ lib/view/room_preparation_view.dart
@@ -8,67 +8,74 @@ class RoomPreparationView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 20),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 40.0, right: 40.0, top: 0, bottom: 25.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.orangeAccent[100],
-                    minimumSize: const Size(double.infinity, double.infinity),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.horizontal(
-                        left: Radius.circular(20),
-                        right: Radius.circular(20),
-                      ),
-                    ),
-                    elevation: 12,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    '部屋を作る',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(fontSize: 30),
-                  ),
-                ),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildMakeRoomButton(context),
+          buildWaitButton(context),
+        ],
+      ),
+    );
+  }
+
+  Widget buildMakeRoomButton(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(
+            left: 40.0, right: 40.0, top: 0, bottom: 25.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.orangeAccent[100],
+            minimumSize: const Size(double.infinity, double.infinity),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(20),
+                right: Radius.circular(20),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 40.0, right: 40.0, bottom: 40.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.blueAccent[100],
-                    minimumSize: const Size(double.infinity, double.infinity),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.horizontal(
-                        left: Radius.circular(20),
-                        right: Radius.circular(20),
-                      ),
-                    ),
-                    elevation: 12,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    '待機する',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(fontSize: 30),
-                  ),
-                ),
+            elevation: 12,
+          ),
+          onPressed: () {},
+          child: Text(
+            '部屋を作る',
+            style:
+                Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildWaitButton(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 40.0, right: 40.0, bottom: 40.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blueAccent[100],
+            minimumSize: const Size(double.infinity, double.infinity),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(20),
+                right: Radius.circular(20),
               ),
             ),
-          ],
+            elevation: 12,
+          ),
+          onPressed: () {},
+          child: Text(
+            '待機する',
+            style:
+                Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 30),
+          ),
         ),
       ),
     );
